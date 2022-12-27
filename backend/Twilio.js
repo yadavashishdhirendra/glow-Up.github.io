8dc8e3a806b216602ef7bbd1d56f0925b1844df6
Ashish Yadav <72142543+yadavashishdhirendra@users.noreply.github.com>
2022-12-27 11:56:10 +0530

    Twilio.js

--- backend/Twilio.js
+++ backend/Twilio.js
@@ -1,11 +1,1 @@
-module.exports = {
-    serviceId: "VAf95e77ae2ff110a72d1d9138b5b419dc",
-    AccountSID: "AC9336676d5ced14775833c38a5bf8b1f2",
-    authToken: "8725a54a6c7645a46cf758228da9a133"
-}
 
-// module.exports = {
-//     serviceId: "VA1c058594b504e81d2fc579fa489b8882",
-//     AccountSID: "AC3d4e092b603c3e99d9586dfcfb937964",
-//     authToken: "280c5953aa089e1789c43905b15fc976"
-// }
